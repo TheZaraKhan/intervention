39f5ab035bfb7e1536dece27261c4da6145561cd
Zara Khan <ZaraMustafa@ymail.com>
2021-09-08 21:15:43 +0500

    signup page 3 & 4 up

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,9 @@ module.exports = {
         main: resolve(__dirname, 'index.html'),
         login: resolve(__dirname, 'pages/login/index.html'),
         signup1: resolve(__dirname, 'pages/signup1/index.html'),
-        signup2: resolve(__dirname, 'pages/signup2/index.html')
+        signup2: resolve(__dirname, 'pages/signup2/index.html'),
+        signup3: resolve(__dirname, 'pages/signup3/index.html'),
+        signup4: resolve(__dirname, 'pages/signup4/index.html')
       }
     }
   }
